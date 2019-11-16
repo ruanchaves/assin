221eb67e9d74ea3b4530cda21f28c51cff032caa
ruanchaves <ruanchaves@gmail.com>
2019-11-16 19:11:34 -0300

    training

---
+++ pipeline.sh
@@ -0,0 +1,15 @@
+python assin-roberta.py ./training_settings/5fold/assin1_ptbr_settings.json
+python assin-roberta.py ./training_settings/5fold/assin1_ptpt_settings.json
+python assin-roberta.py ./training_settings/5fold/assin2_settings.json
+
+python assin-roberta.py ./training_settings/10fold/assin1_ptbr_settings.json
+python assin-roberta.py ./training_settings/10fold/assin1_ptpt_settings.json
+python assin-roberta.py ./training_settings/10fold/assin2_settings.json
+
+python assin-roberta.py ./training_settings/50fold/assin1_ptbr_settings.json
+python assin-roberta.py ./training_settings/50fold/assin1_ptpt_settings.json
+python assin-roberta.py ./training_settings/50fold/assin2_settings.json
+
+python assin-roberta.py ./training_settings/100fold/assin1_ptbr_settings.json
+python assin-roberta.py ./training_settings/100fold/assin1_ptpt_settings.json
+python assin-roberta.py ./training_settings/100fold/assin2_settings.json
