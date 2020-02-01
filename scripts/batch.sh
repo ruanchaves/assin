f1fdc8c76f37b0b306837bd46dcc28ae038750ea
ruanchaves <ruanchaves@gmail.com>
2020-02-01 15:53:17 -0300

    test - gpu check

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -6,16 +6,31 @@ for dataset in ${datasets[@]}; do
     for filename in ${config_files[@]}; do
         single_filename="${filename##*/}"
 
-        for i in $(seq 0 7); do
-            status=$(nvidia smi -i $i | grep 'No running processes found')
-            echo $status
+        chosen_gpu=-1
+        while [[ $chosen_gpu -lt 0 ]]
+        do
+            for i in $(seq 0 7); do
+                status=$(nvidia smi -i $i | grep "No running processes found")
+                if [[ -z $status ]]
+                then
+                    echo "Skipping GPU $i ."
+                else
+                    echo "Running on GPU $i ."
+                    chosen_gpu=$i
+            done
+            if [[ $chosen_gpu -lt 0 ]]
+            then
+                sleep 5
+            fi
         done
 
         # sudo nvidia-docker run \
         #     -v `pwd`:/home \
         #     --env DATASET=$dataset \
         #     --env CONFIG=$single_filename \
-        #     --env CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
-        #     -it --rm ruanchaves/assin:2.0 bash /home/scripts/run_assin.sh
+        #     --env CUDA_VISIBLE_DEVICES=$chosen_gpu \
+        #     -it --rm ruanchaves/assin:2.0 bash /home/scripts/run_assin.sh &
+
+        chosen_gpu=-1
     done
 done
