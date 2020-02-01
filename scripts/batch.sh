ee20f97e62ae53989b2f99876e8eee70f019a5ff
ruanchaves <ruanchaves@gmail.com>
2020-02-01 14:07:22 -0300

    automation

---
+++ scripts/batch.sh
@@ -0,0 +1,15 @@
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ..
+datasets=("assin2" "assin-ptpt" "assin-ptbr")
+config_files=(settings/*.yml)
+for dataset in ${datasets[@]}; do
+    for filename in ${config_files[@]}; do
+        single_filename="${filename##*/}"
+        sudo nvidia-docker run \
+            -v `pwd`:/home \
+            --env DATASET=$dataset \
+            --env CONFIG=$single_filename \
+            --env CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+            -it --rm ruanchaves/assin:2.0 bash /home/scripts/run_assin.sh
+    done
+done
